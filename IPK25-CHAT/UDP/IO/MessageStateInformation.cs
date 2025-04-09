edd58242518d7df8cf39ae559d3fc2d24d5db2a6
Igor Lacko <iginonajlepsi@gmail.com>
2025-04-09 15:24:43 +0000

    feat: added (hopefully) full implementation of the UDPServerCommunicator class. Also added a structure for the current state of sent messages (in the context of confirmation) which helps the communicator class

---
+++ IPK25-CHAT/UDP/IO/MessageStateInformation.cs
@@ -0,0 +1,26 @@
+/* Class that contains the current state of a UDP message */
+
+namespace IPK_25_CHAT.UDP.IO;
+
+/// <summary>
+/// Class that contains the current state of a UDP message sent from the client.
+/// </summary>
+/// <param name="messageId">The message ID of the sent message.</param>
+/// <param name="onConfirm">Task completion source for confirmation.</param>
+public struct MessageStateInformation(ushort messageId, TaskCompletionSource<bool> onConfirm)
+{
+    /// <summary>
+    /// The message ID of the sent message.
+    /// </summary>
+    public ushort MessageID = messageId;
+
+    /// <summary>
+    /// Set to true after confirmation is received.
+    /// </summary>
+    public TaskCompletionSource<bool> OnConfirm = onConfirm;
+
+    /// <summary>
+    /// Task that is completed when the message is confirmed.
+    /// </summary>
+    public Task<bool> MessageConfirmed = onConfirm.Task;
+}
--- IPK25-CHAT/UDP/IO/UDPServerCommunicator.cs
+++ IPK25-CHAT/UDP/IO/UDPServerCommunicator.cs
@@ -2,6 +2,7 @@
 
 namespace IPK_25_CHAT.UDP.IO;
 
+using IPK_25_CHAT.Enum;
 using IPK_25_CHAT.Interface;
 using IPK_25_CHAT.Message;
 using System.Net;
@@ -12,17 +13,38 @@ using System.Net.Sockets;
 /// </summary>
 /// <param name="host">The host IP address.</param>
 /// <param name="initialPort">The initial port number.</param>
-public class UDPServerCommunicator(IPAddress host, ushort initialPort) : IServerCommunicator
+/// <param name="timeout">Timeout for receiving CONFIRM messages.</param>
+public class UDPServerCommunicator(IPAddress host, ushort initialPort, ushort timeout, ushort retransmissions) : IServerCommunicator
 {
     /// <summary>
     /// The host IP address.
     /// </summary>
     private readonly IPAddress Host = host;
 
-    /// <summary>
+    /// <summary>0
     /// The current port number.
     /// </summary>
     private ushort Port = initialPort;
+
+    /// <summary>
+    /// The timeout for receiving CONFIRM messages.
+    /// </summary>
+    private readonly ushort Timeout = timeout;
+
+    /// <summary>
+    /// The number of retransmissions for the UDP client.
+    /// </summary>
+    private readonly ushort NumberOfRetransmissions = retransmissions;
+
+    /// <summary>
+    /// List of already seen message IDs.
+    /// </summary>
+    private readonly List<ushort> SeenMessageIDs = [];
+
+    /// <summary>
+    /// Dictionary mapping sent message ID's to their current states.
+    /// </summary>
+    private readonly Dictionary<ushort, MessageStateInformation> SentMessageInformation = [];
 
     /// <summary>
     /// Socket for communication.
@@ -30,46 +52,219 @@ public class UDPServerCommunicator(IPAddress host, ushort initialPort) : IServer
     private Socket? UdpSocket = null;
 
     /// <summary>
+    /// Semaphore for sending messages. Ensures that a sent message is confirmed before sending another.
+    /// </summary>
+    private readonly SemaphoreSlim SendGuardian = new(1, 1);
+
+    /// <summary>
     /// Current message ID, starting from 0.
     /// </summary>
-    private short currentMessageId = 0;
+    private ushort currentMessageId = 0;
+
+    /// <summary>
+    /// The event thrown when a message is received from the server when receiving in a loop.
+    /// </summary>
+    public event Action<Message> MessageReceived = message => { };
+
+    /// <summary>
+    /// The event throuwn when a message confirmation timeouts.
+    /// </summary>
+    public event Action ConfirmTimeouted = () => { };
+
+    /// <summary>
+    /// Cancellation token source. Cancels the server communicator at the end of the program.
+    /// </summary>
+    public CancellationTokenSource ServerInputCancellationToken { get; } = new();
 
     /// <summary>
     /// Initializes the socket for initial connection.
     /// </summary>
-    public void Initialize() => UdpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    public void Initialize()
+    {
+        UdpSocket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        UdpSocket.Bind(new IPEndPoint (IPAddress.Any, 0));
+    }
 
     /// <summary>
     /// Closes the socket.
     /// </summary>
     public void Close()
     {
+        // Already closed
+        if(ServerInputCancellationToken.IsCancellationRequested) return;
+
+        // Close
+        ServerInputCancellationToken.Cancel();
         UdpSocket!.Shutdown(SocketShutdown.Both);
         UdpSocket!.Close();
     }
 
     /// <summary>
-    /// Sends the initial message to the server.
+    /// Runs the server communicator.
+    /// </summary>
+    public void Run() => Task.Run(async () =>
+    {
+        while(!ServerInputCancellationToken.IsCancellationRequested)
+        {
+            // Buffer to store the response, use the max MTU size
+            byte[] buffer = new byte[1500];
+
+            // Filter out depending on if the socket is connected or not
+            if(!UdpSocket!.Connected)
+            {
+                // Endpoint to store
+                IPEndPoint remoteEndPoint = new(IPAddress.Any, 0);
+
+                // Receive the message
+                await UdpSocket!.ReceiveFromAsync(buffer, remoteEndPoint);
+
+                // Filter out non-server IP addresses at the beginning
+                if(remoteEndPoint.Address != Host) continue;
+
+                // Update the port if the server switched (on the initial AUTH) and connect to only receive from the server
+                if(remoteEndPoint.Port != Port)
+                {
+                    // Reset port (todo remove)
+                    Port = (ushort)remoteEndPoint.Port;
+
+                    // Use connected UDP socket to only receive messages from the server
+                    UdpSocket!.Connect(new IPEndPoint(Host, Port));
+                }
+            }
+
+            // If the socket is connected, we can just use ReceiveAsync()
+            else await UdpSocket!.ReceiveAsync(buffer);
+
+            // Parse the mesasge and handle it
+            await HandleReceivedMessage(Message.Parse(buffer));
+        }
+    });
+
+    /// <summary>
+    /// Handler method for received messages.
+    /// Needed here as opposed to TCPServerCommunicator, since the latter just delegates all messages to the client.
+    /// Here we need to handle CONFIRM and PING internally, while checking for already seen message IDs. 
+    /// </summary>
+    /// <param name="message">The message to handle.</param>
+    public async Task HandleReceivedMessage(Message message)
+    {
+        // No checks needed here
+        if(message.Type == MessageType.MALFORMED)
+        {
+            MessageReceived.Invoke(message);
+            return;
+        }
+
+        // CONFIRM received --> Set the task result for the confirmed message
+        else if(message.Type == MessageType.CONFIRM)
+        {
+            SentMessageInformation[message.GetMessageID()].OnConfirm.SetResult(true);
+            SentMessageInformation.Remove(message.GetMessageID());
+        }
+
+        // Check if the ID was seen already, if yes send confirm and return
+        else if(SeenMessageIDs.Contains(message.GetMessageID()))
+        {
+            await SendConfirm(message.GetMessageID());
+            return;
+        }
+
+        // PING received --> Send CONFIRM
+        else if(message.Type == MessageType.PING)
+        {
+            ushort messageID = message.GetMessageID();
+            SeenMessageIDs.Add(messageID);
+            await SendConfirm(messageID);
+            return;
+        }
+
+        // Non PING/CONFIRM message which was also not seen before --> delegate to the client class
+        else
+        {
+            SeenMessageIDs.Add(message.GetMessageID());
+            MessageReceived.Invoke(message);
+        }
+    }
+
+    /// <summary>
+    /// Sends a CONFIRM message to the server.
+    /// Kept separate from the SendMessage method, since we don't need to wait for CONFIRM to be, well, confirmed.
+    /// </summary>
+    /// <param name="messageID">ID of the message to confirm.</param>
+    public async Task SendConfirm(ushort messageID)
+    {
+        await SendGuardian.WaitAsync();
+
+        // Send confirm
+        if(UdpSocket!.Connected) await UdpSocket.SendAsync(new ConfirmMessage(messageID).AsBytes(messageID));
+        else await UdpSocket!.SendToAsync(new ConfirmMessage(messageID).AsBytes(messageID), new IPEndPoint(Host, Port));
+
+        // Release the semaphore
+        SendGuardian.Release();
+    }
+
+    /// <summary>
+    /// Sends a message to the server.
     /// Needs to be split from the other messages due to the dynamic port switching.
     /// </summary>
-    /// <param name="message">Initial AUTH message to send.</param>
-    public async Task SendInitialMessage(AuthMessage message)
+    /// <param name="message">Message to send.</param>
+    /// <returns>True if the message was confirmed, false otherwise.</returns>
+    public async Task SendMessage(Message message)
     {
-        // Initial endpoint
-        IPEndPoint initial = new(Host, Port);
+        // Wait for access to sending
+        await SendGuardian.WaitAsync();
+
+        // Current message ID
+        ushort messageID = currentMessageId++;
+
+        // Create a object for the state of the message
+        MessageStateInformation messageState = new(messageID, new TaskCompletionSource<bool>());
+        SentMessageInformation[messageID] = messageState;
+
+        // Current destination
+        IPEndPoint current = new(Host, Port);
 
         // Send the message
-        await UdpSocket!.SendToAsync(message.AsBytes(currentMessageId++), SocketFlags.None, initial);
+        byte[] messageAsBytes = message.AsBytes(messageID);
 
-        // Receive CONFIRM
-        byte[] buffer = new byte[1500]; // Max MTU size
-        int received = (await UdpSocket!.ReceiveFromAsync(buffer, initial)).ReceivedBytes;
+        // Depending on if we were connected or not
+        if(UdpSocket!.Connected) await UdpSocket.SendAsync(messageAsBytes);
+        else await UdpSocket!.SendToAsync(messageAsBytes, current);
 
-        // None received? TODO: Find out how to handle this.
-        if(received == 0)
+        // Flag
+        bool confirmed = false;
+
+        // Wait for confirmation for RETRANSMISSIONS number of times
+        for(int i = 0; i < NumberOfRetransmissions; i++)
         {
-            Console.WriteLine("No data received from server.");
-            Environment.Exit(1);
+            Task<bool> completedTask = messageState.MessageConfirmed;
+            Task timeoutTask = Task.Delay(Timeout);
+            Task completed = await Task.WhenAny(completedTask, timeoutTask);
+
+            // Message was confirmed
+            if(completed == completedTask)
+            {
+                confirmed = true;
+                break;
+            }
+
+            // Again, send depending on the state of the socket connection
+            else
+            {
+                // Send the message again
+                if(UdpSocket!.Connected) await UdpSocket.SendAsync(messageAsBytes);
+                else await UdpSocket!.SendToAsync(messageAsBytes, current);
+            }
         }
+
+        // Invoke the timeout event if the message was not confirmed and close the connection
+        if(!confirmed)
+        {
+            UdpSocket!.Close();
+            ConfirmTimeouted.Invoke();
+        }
+
+        // Else release the semaphore
+        else SendGuardian.Release();
     }
 }
